d05f8d57105c6d434c7561c12fd046aa0f21e276
Declan McBride <2399448m@student.gla.ac.uk>
2021-11-29 13:30:15 +0000

    WIP: Implemented TabBar on Homepage - works, but is not coloured correctly.
    Updated top_bar.dart to ensure it is always the correct colour.

--- fic_flutter/lib/main.dart
+++ fic_flutter/lib/main.dart
@@ -45,89 +45,97 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: const TopBar(page: 'Home'),
-      drawer: Drawer(
-        // Add a ListView to the drawer. This ensures the user can scroll
-        // through the options in the drawer if there isn't enough vertical
-        // space to fit everything.
-        child: ListView(
-          // Important: Remove any padding from the ListView.
-          padding: EdgeInsets.zero,
-          children: [
-            const DrawerHeader(
-              decoration: BoxDecoration(
-                color: Colors.blue,
-              ),
-              child: Text('Drawer Header'),
+    return MaterialApp(
+    home: DefaultTabController (
+        length: 2,
+        child: Scaffold(
+          appBar: const TopBar(page: 'Home'),
+          drawer: Drawer(
+            // Add a ListView to the drawer. This ensures the user can scroll
+            // through the options in the drawer if there isn't enough vertical
+            // space to fit everything.
+            child: ListView(
+              // Important: Remove any padding from the ListView.
+              padding: EdgeInsets.zero,
+              children: [
+                const DrawerHeader(
+                  decoration: BoxDecoration(
+                    color: Colors.green,
+                  ),
+                  child: Text('Drawer Header'),
+                ),
+                ListTile(
+                  title: const Text('Item 1'),
+                  onTap: () {
+                    // Update the state of the app
+                    // ...
+                    // Then close the drawer
+                    Navigator.pop(context);
+                  },
+                ),
+                ListTile(
+                  title: const Text('Item 2'),
+                  onTap: () {
+                    // Update the state of the app
+                    // ...
+                    // Then close the drawer
+                    Navigator.pop(context);
+                  },
+                ),
+              ],
             ),
-            ListTile(
-              title: const Text('Item 1'),
-              onTap: () {
-                // Update the state of the app
-                // ...
-                // Then close the drawer
-                Navigator.pop(context);
-              },
+          ),
+          body: Center(
+            // Center is a layout widget. It takes a single child and positions it
+            // in the middle of the parent.
+            child: Column(
+              // Column is also a layout widget. It takes a list of children and
+              // arranges them vertically. By default, it sizes itself to fit its
+              // children horizontally, and tries to be as tall as its parent.
+              //
+              // Invoke "debug painting" (press "p" in the console, choose the
+              // "Toggle Debug Paint" action from the Flutter Inspector in Android
+              // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+              // to see the wireframe for each widget.
+              //
+              // Column has various properties to control how it sizes itself and
+              // how it positions its children. Here we use mainAxisAlignment to
+              // center the children vertically; the main axis here is the vertical
+              // axis because Columns are vertical (the cross axis would be
+              // horizontal).
+              //mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Column(
+                  children: const <Widget> [
+                    TabBar(
+                        tabs: [
+                          Tab(text:"Resources"),
+                          Tab(text: "Species")
+                        ]
+                    ),
+                  ],
+                ),
+                Padding(
+                    padding: const EdgeInsets.fromLTRB(0, 30, 0, 0),
+                    child: Image.asset('assets/images/word art (low res).png')),
+                const Text(
+                  'Flock Information Centre',
+                  style: TextStyle(fontSize: 30),
+                ),
+                const Padding(
+                  padding: EdgeInsets.all(8),
+                  child: Text(
+                    'Online resources to provide information to professionals in the farming industry',
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                const NavigationButton(title: 'General Resources'),
+                const NavigationButton(title: 'Benefits of Reducing Losses'),
+              ],
             ),
-            ListTile(
-              title: const Text('Item 2'),
-              onTap: () {
-                // Update the state of the app
-                // ...
-                // Then close the drawer
-                Navigator.pop(context);
-              },
-            ),
-          ],
-        ),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            // TODO: Insert tabs here
-            Padding(
-                padding: const EdgeInsets.fromLTRB(0, 30, 0, 0),
-                child: Image.asset('assets/images/word art (low res).png')),
-            const Text(
-              'Flock Information Centre',
-              style: TextStyle(fontSize: 30),
-            ),
-            const Padding(
-              padding: EdgeInsets.all(8),
-              child: Text(
-                'Online resources to provide information to professionals in the farming industry',
-                textAlign: TextAlign.center,
-              ),
-            ),
-            const NavigationButton(title: 'General Resources'),
-            const NavigationButton(title: 'Benefits of Reducing Losses'),
-          ],
-        ),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+          ), // This trailing comma makes auto-formatting nicer for build methods.
+        )
+      )
     );
   }
 }

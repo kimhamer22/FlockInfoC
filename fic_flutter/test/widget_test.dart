3ecb1187db8b277c149c21fdaba6fc8bea312d67
Kārlis Siders <2467273s@student.gla.ac.uk>
2021-12-26 15:43:42 +0000

    Delete default test, add test to click Species (#57)

--- fic_flutter/test/widget_test.dart
+++ fic_flutter/test/widget_test.dart
@@ -5,26 +5,23 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
+import 'package:fic_flutter/pages/home_species.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:fic_flutter/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Clicking Species shows Sheep button',
+      (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const FlockControl());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
     // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
+    await tester.tap(find.byWidget(const HomeSpecies()));
+    //await tester.tap(find.byIcon(Icons.add));
     await tester.pump();
 
     // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.text('Sheep'), findsOneWidget);
   });
 }

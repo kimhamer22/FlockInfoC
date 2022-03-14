cb3379f5fe70bbaae2aa88e37b58cb1fb0280293
Declan McBride <2399448m@student.gla.ac.uk>
2022-03-14 11:37:51 +0000

    #94 - Home Page Tests added

--- fic_flutter/test/home_page_test.dart
+++ fic_flutter/test/home_page_test.dart
@@ -2,18 +2,48 @@ import 'package:fic_flutter/pages/home_resources.dart';
 import 'package:fic_flutter/pages/home_species.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:fic_flutter/main.dart';
 
 void main() {
   testWidgets('Home page has both tabs', (WidgetTester tester) async {
-    //This hasn't been working because you can't test entire pages. Need to build the actual TabBar as a widget here to test it.
+    await tester.pumpWidget(MaterialApp(
+        home: DefaultTabController(
+      length: 2,
+      child: Column(children: <Widget>[
+        Container(
+          constraints: const BoxConstraints(maxHeight: 150.0),
+          child: const Material(
+            child: TabBar(
+              labelColor: Colors.black,
+              labelStyle: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
+              indicator: BoxDecoration(
+                color: Color(0x80DBF9D3),
+              ),
+              tabs: [
+                Tab(text: "Home"),
+                Tab(text: "Species"),
+              ],
+            ),
+          ),
+        ),
+        const Expanded(
+          child: TabBarView(
+            children: <Widget>[
+              HomeResources(),
+              HomeSpecies(),
+            ],
+          ),
+        ),
+      ]),
+    )));
+    await tester.pumpAndSettle();
 
-//   // Tap the 'Species' tab and trigger a frame.
-//   await tester.tap(find.text('Species'));
-//   //await tester.tap(find.byIcon(Icons.add));
-//   await tester.pump();
-//
-//   // Verify that page has a Sheep widget.
-//   expect(find.text('Sheep'), findsOneWidget);
+    final homeTabFinder = find.text("Home");
+    final speciesTabFinder = find.text("Species");
+
+    expect(homeTabFinder, findsOneWidget);
+    expect(speciesTabFinder, findsOneWidget);
   });
 }
